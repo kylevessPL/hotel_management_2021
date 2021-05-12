492ab51e73de15e6ac087f8cdd85b183a03dadf9
geekon <kacperpiasta@gmail.com>
2021-05-12 23:43:22 +0200

    recreate liquibase changelogs, adjust db tables to fit project requirements

---
+++ application/web-application-parent/web-application-infrastructure/src/main/java/pl/piasta/hotel/infrastructure/model/DiscountsEntity.java
@@ -0,0 +1,29 @@
+package pl.piasta.hotel.infrastructure.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "discounts")
+@Getter
+@Setter
+public class DiscountsEntity {
+
+    @Id @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "discounts_generator")
+    @SequenceGenerator(name="discounts_generator", sequenceName = "seq_discounts", allocationSize = 1)
+    private Integer id;
+    @Column(name = "code", nullable = false, length = 20)
+    private String code;
+    @Column(name = "value")
+    private Integer value;
+
+}
